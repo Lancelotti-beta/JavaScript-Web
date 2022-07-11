ef6b6472b9ac053f57956a56337a5e2e6673da2a
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-11 20:13:36 -0300

    Colocanando iten '.html'| Iniciando localStorage
    Criando uma function para criar a <li/> e <strong/>, colocar o conteudo
    da <strong/> na <li/> | Começar a Usar o localStorage no projeto

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 
 
 const form = document.getElementById('novoItem')
+const lista = document.getElementById('lista')
+
+const arrayLocal = []
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    
-    console.log(e.target.elements['nome'].value)
-    console.log(e.target.elements['quantidade'].value)
+
+    const nome = e.target.elements['nome']
+    const quantidade = e.target.elements['quantidade']
+
+    lista.appendChild(criaElementoDaLista(nome.value, quantidade.value))
+
+    nome.value = ""
+    quantidade.value = ""
 })
+
+function criaElementoDaLista(nome, quantidade) {
+    //<li class="item"><strong></strong></li>
+    const li = document.createElement('li')
+    li.classList.add('item')
+    const strong = document.createElement('strong')
+    strong.innerHTML = quantidade
+
+    li.appendChild(strong)
+    //concatenando elemento <strong/> com <li/>
+    li.innerHTML += nome
+
+    armazenaItensNoLocalStorage(nome, quantidade)
+    return li
+}
+
+function armazenaItensNoLocalStorage(nome, quantidade) {
+    arrayLocal.push(localStorage.setItem('item', nome))
+    arrayLocal.push(localStorage.setItem('quantidade', quantidade))
+}
